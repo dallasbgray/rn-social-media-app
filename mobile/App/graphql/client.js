baecac5df1de02bb4c429f218387576fa23bbe19
Dallas Gray <36283741+dallasbgray@users.noreply.github.com>
2021-03-10 12:21:58 -0800

    Add files via upload

---
+++ mobile/App/graphql/client.js
@@ -0,0 +1,13 @@
+
+import ApolloClient from 'apollo-boost';
+
+export const client = new ApolloClient({
+    uri: 'http://192.168.128.1:4000',
+    request: operation => {
+        operation.setContext({
+            headers: {
+                Authorization: 'Bearer 123',
+            },
+        });
+    },
+});
